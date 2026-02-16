1e66cfe35d5f19d88779b25d133a9dfc3ff164d8
OmKadam-1 <kom110798@gmail.com>
2026-02-16 18:36:50 +0530

    authorizerole function

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -19,3 +19,11 @@ export const authenticateJWT = (req, res, next) => {
     return res.status(403).json({ message: "Invalid token" });
   }
 };
+
+export const authorizeRole = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role))
+      return res.status(403).json({ message: "Access denied" });
+    next();
+  };
+};
