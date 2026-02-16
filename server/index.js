e815f91b2d70dcd675db3878163b37f3e960c494
OmKadam-1 <kom110798@gmail.com>
2026-02-16 18:48:41 +0530

    craete-admin function created

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import connectDB from "./db";
+import User from "./models/User";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 dotenv.config();
 
@@ -10,6 +13,33 @@ app.use(express.json());
 app.use(cors());
 
 const PORT = process.env.PORT || 8080;
+
+const createAdmin = async () => {
+  try {
+    const existingAdmin = await User.findOne({
+      email: process.env.ADMIN_EMAIL
+    });
+
+    if (existingAdmin) return;
+
+    const hashedPassword = await bcrypt.hash(
+      process.env.ADMIN_PASSWORD,
+      10
+    );
+
+    await User.create({
+      email: process.env.ADMIN_EMAIL,
+      password: hashedPassword,
+      role: "ADMIN"
+    });
+
+    console.log("Admin created successfully");
+  } catch (error) {
+    console.error("Admin creation error:", error.message);
+  }
+};
+
+
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome" });
