74bd38998caa270b01d4c9141cb01d6fdf8b7f54
Pratik Salunke <pratiksalunke3112@gmail.com>
2026-02-17 10:06:00 +0530

    MyAppoinment page is added for viewing status of my booked appoinment

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,18 +4,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./views/Login";
 import Signup from "./views/Signup";
 import BookAppointment from "./views/patient/BookAppointment";
+import ManageAppointment from "./views/doctor/ManageAppointment";
 
 const root = createRoot(document.getElementById("root"));
 
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/patient/book" element={<BookAppointment />} />
-  <Route path="/doctor/manage-appointments" element={<ManageAppointment />} />
-
+      <Route
+        path="/doctor/manage-appointments"
+        element={<ManageAppointment />}
+      />
     </Routes>
   </BrowserRouter>,
 );
