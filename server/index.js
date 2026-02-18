80f138665584e7dafdcee9267b5d66d50897eebd
OmKadam-1 <kom110798@gmail.com>
2026-02-18 11:11:17 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -4,7 +4,8 @@ import express from "express";
 import connectDB from "./db.js";
 import User from "./models/User.js";
 import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
+
+import { registerPatient,loginUser } from "./controllers/authController.js";
 import { postAppointment, getPatientAppointments, getDoctorAppointments, approveAppointment, rejectAppointment } from "./controllers/appointment.js";
 
 dotenv.config();
@@ -48,56 +49,9 @@ app.get("/health", (req, res) => {
   res.json({ status: "OK" });
 });
 
-app.post("/api/auth/register", async (req, res) => {
-  try {
-    const { email, password } = req.body;
+app.post("/api/auth/register", registerPatient);
 
-    const existingUser = await User.findOne({ email });
-    if (existingUser)
-      return res.status(400).json({ error: "User already exists" });
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    await User.create({
-      email,
-      password: hashedPassword,
-      role: "PATIENT"
-    });
-
-    res.status(201).json({ message: "Registration successful" });
-
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-app.post("/api/auth/login", async (req, res) => {
-  try {
-    const { email, password } = req.body;
-
-    const user = await User.findOne({ email });
-    if (!user)
-      return res.status(400).json({ error: "User not found" });
-
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch)
-      return res.status(400).json({ error: "Invalid password" });
-
-    const token = jwt.sign(
-      { id: user._id, role: user.role },
-      process.env.JWT_SECRET,
-      { expiresIn: "1d" }
-    );
-
-    res.json({
-      token,
-      role: user.role
-    });
-
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
+app.post("/api/auth/login",loginUser);
 
 
 
