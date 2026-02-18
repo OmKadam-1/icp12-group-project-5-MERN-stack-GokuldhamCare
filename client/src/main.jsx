828f77ea6abff86d6022bb6f7703e5a7fee07e45
OmKadam-1 <kom110798@gmail.com>
2026-02-18 11:36:46 +0530

    routes added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,8 @@ import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signup from "./views/auth/Signup";
 import BookAppointment from "./views/patient/BookAppointment";
+import DoctorDashboard from "./views/doctor/DoctorDashboard";
+import PatientDashboard from "./views/patient/PatientDashboard";
 import ManageAppointment from "./views/doctor/ManageAppointment";
 import MyAppointments from "./views/patient/MyAppointment";
 
@@ -13,6 +15,8 @@ root.render(
     <Routes>
       <Route path="/signup" element={<Signup />} />
       <Route path="/patient/book" element={<BookAppointment />} />
+       <Route path="/doctor/dashboard" element={<DoctorDashboard />} />
+        <Route path="/" element={<PatientDashboard />} />
       <Route
         path="/doctor/manage-appointments"
         element={<ManageAppointment />}
